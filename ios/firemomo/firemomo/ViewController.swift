b494f86589afda1de886fa9b367cbca98fe51d29
Alan Escobar <alan.escobar.martinez@gmail.com>
2018-07-17 01:07:33 -0500

    Implement the FirebaseUI demo

--- ios/firemomo/firemomo/ViewController.swift
+++ ios/firemomo/firemomo/ViewController.swift
@@ -8,48 +8,67 @@
 
 import UIKit
 import Firebase
-import GoogleSignIn
-import FBSDKLoginKit
+import FirebaseUI
 
-class ViewController: UIViewController, GIDSignInUIDelegate, FBSDKLoginButtonDelegate {
-    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
-        if let error = error {
-            print(error.localizedDescription)
-            return
-        }
-        else {
-            print("User id logged in: \(result.token.userID!)")
-            let credential = FacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
-            Auth.auth().signInAndRetrieveData(with: credential) { (authResult, error) in
-                if let error = error {
-                    print("Facebook error: \(error)")
-                    return
-                }
-                print("Facebook Logged in: \(authResult!)")
+let kFirebaseTermsOfService = URL(string: "https://firebase.google.com/terms/")!
+
+class ViewController: UIViewController, FUIAuthDelegate{
+    @IBOutlet weak var btnLogout: UIButton!
+    
+    fileprivate(set) var auth:Auth?
+    fileprivate(set) var authUI: FUIAuth? //only set internally but get externally
+    fileprivate(set) var authStateListenerHandle: AuthStateDidChangeListenerHandle?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+        self.auth = Auth.auth()
+        self.authUI = FUIAuth.defaultAuthUI()
+        self.authUI?.delegate = self
+        self.authUI?.providers = [FUIGoogleAuth(), FUIFacebookAuth(),]
+        
+        self.authUI?.tosurl = kFirebaseTermsOfService
+        
+        self.authUI?.customStringsBundle = Bundle.main
+        
+        self.authStateListenerHandle = self.auth?.addStateDidChangeListener { (auth, user) in
+            guard user != nil else {
+                self.loginAction(sender: self)
+                return
             }
         }
     }
     
-    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
-        print("Logout")
-    }
-    
-    @IBOutlet weak var btnGoogleSignIn: GIDSignInButton!
-    @IBOutlet weak var btnFacebookLogin: FBSDKLoginButton!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        GIDSignIn.sharedInstance().uiDelegate = self
-        btnFacebookLogin.delegate = self
-        btnFacebookLogin.readPermissions = ["email", "public_profile"]
-        btnGoogleSignIn.style = .wide
-        
-    }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-
-
+    
+    @IBAction func loginAction(sender: AnyObject) {
+        // Present the default login view controller provided by authUI
+        let authViewController = authUI?.authViewController();
+        self.present(authViewController!, animated: true, completion: nil)
+        
+    }
+    
+    func authUI(_ authUI: FUIAuth, didSignInWith user: User?, error: Error?) {
+        guard let authError = error else { return }
+        
+        let errorCode = UInt((authError as NSError).code)
+        
+        switch errorCode {
+        case FUIAuthErrorCode.userCancelledSignIn.rawValue:
+            print("User cancelled sign-in");
+            break
+            
+        default:
+            let detailedError = (authError as NSError).userInfo[NSUnderlyingErrorKey] ?? authError
+            print("Login error: \((detailedError as! NSError).localizedDescription)");
+        }
+    }
+    
+    @IBAction func actnLogout(_ sender: UIButton) {
+        try! auth!.signOut()
+    }
+    
 }
-
