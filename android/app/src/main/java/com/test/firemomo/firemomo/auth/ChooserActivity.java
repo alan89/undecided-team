0c4d94d964fa54d70544511bdbfc9c9d456f865b
Sam <samstern@google.com>
2018-07-18 10:34:28 -0500

    Restyle Chooser Activity

--- android/app/src/main/java/com/test/firemomo/firemomo/auth/ChooserActivity.java
+++ android/app/src/main/java/com/test/firemomo/firemomo/auth/ChooserActivity.java
@@ -1,85 +1,33 @@
 package com.test.firemomo.firemomo.auth;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
 
 import com.test.firemomo.firemomo.R;
 
-
-public class ChooserActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
-
-    private static final Class[] CLASSES = new Class[]{
-            GoogleSignInActivity.class,
-            FacebookLoginActivity.class,
-
-    };
-
-    private static final int[] DESCRIPTION_IDS = new int[] {
-            R.string.desc_google_sign_in,
-            R.string.desc_facebook_login,
-
-    };
+public class ChooserActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chooser);
 
-        // Set up ListView and Adapter
-        ListView listView = findViewById(R.id.list_view);
+        findViewById(R.id.button_launch_google_flow)
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        startActivity(new Intent(ChooserActivity.this, GoogleSignInActivity.class));
+                    }
+                });
 
-        MyArrayAdapter adapter = new MyArrayAdapter(this, android.R.layout.simple_list_item_2, CLASSES);
-        adapter.setDescriptionIds(DESCRIPTION_IDS);
-
-        listView.setAdapter(adapter);
-        listView.setOnItemClickListener(this);
-    }
-
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        Class clicked = CLASSES[position];
-        startActivity(new Intent(this, clicked));
-    }
-
-    public static class MyArrayAdapter extends ArrayAdapter<Class> {
-
-        private Context mContext;
-        private Class[] mClasses;
-        private int[] mDescriptionIds;
-
-        public MyArrayAdapter(Context context, int resource, Class[] objects) {
-            super(context, resource, objects);
-
-            mContext = context;
-            mClasses = objects;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            View view = convertView;
-
-            if (convertView == null) {
-                LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(LAYOUT_INFLATER_SERVICE);
-                view = inflater.inflate(android.R.layout.simple_list_item_2, null);
-            }
-
-            ((TextView) view.findViewById(android.R.id.text1)).setText(mClasses[position].getSimpleName());
-            ((TextView) view.findViewById(android.R.id.text2)).setText(mDescriptionIds[position]);
-
-            return view;
-        }
-
-        public void setDescriptionIds(int[] descriptionIds) {
-            mDescriptionIds = descriptionIds;
-        }
+        findViewById(R.id.button_launch_facebook_flow)
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        startActivity(new Intent(ChooserActivity.this, FacebookLoginActivity.class));
+                    }
+                });
     }
 }
